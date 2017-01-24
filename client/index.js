f461d5e0927ae14e59c1204c1d19358b63fc4eb2
Don Smith <locksmithdon@gmail.com>
2017-01-25 00:57:52 +1300

    add redux tooling

--- client/index.js
+++ client/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import { Provider } from 'react-redux'
 
 import App from './components/App'
-import reducer from './reducers'
+import reducers from './reducers'
 
 let store = createStore(
-  reducer,
-  applyMiddleware(thunk)
+  reducers, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 document.addEventListener('DOMContentLoaded', () => {
