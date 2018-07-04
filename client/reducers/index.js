6de3dc4fb600302f441c5890ff91821ef26bb78a
Don Smith <don@antiantonym.com>
2018-07-04 13:40:32 +1200

    import constant action names into the reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,37 +1,46 @@
-const INITIAL_STATE = {
+import {
+  TOGGLE_COLOR,
+  ADD_NEW_COLOR,
+  SAVING_NEW_COLOR,
+  SAVED_NEW_COLOR,
+  UPDATE_COLOR,
+  REQUESTING_COLOR
+} from '../actions'
+
+const initialState = {
   color: '',
   isWaitingOnApi: false,
   isColorFormVisible: false
 }
 
-const reducer = (state = INITIAL_STATE, action) => {
+const reducer = (state = initialState, action) => {
   const newState = Object.assign({}, state)
 
   switch (action.type) {
-    case 'TOGGLE_COLOR':
+    case TOGGLE_COLOR:
       newState.isColorFormVisible = !newState.isColorFormVisible
       return newState
 
-    case 'ADD_NEW_COLOR':
+    case ADD_NEW_COLOR:
       newState.isColorFormVisible = false
       newState.color = action.color
       return newState
 
-    case 'SAVING_NEW_COLOR':
+    case SAVING_NEW_COLOR:
       newState.isWaitingOnApi = true
       return newState
 
-    case 'SAVED_NEW_COLOR':
+    case SAVED_NEW_COLOR:
       newState.isWaitingOnApi = false
       newState.color = action.color
       return newState
 
-    case 'UPDATE_COLOR':
+    case UPDATE_COLOR:
       newState.color = action.color
       newState.isWaitingOnApi = false
       return newState
 
-    case 'REQUESTING_COLOR':
+    case REQUESTING_COLOR:
       newState.color = ''
       newState.isWaitingOnApi = true
       return newState
